9af2b0af601ff725f14b0a098a3f425720ed2bef
Diego <d.solis.sm@gmail.com>
2021-04-30 15:45:43 -0400

    ProductManagment: Java Currency and Multithreading

---
+++ ProductManagmentJavaCurrencyAndMultithreading/src/labs/pm/data/ProductManagerException.java
@@ -0,0 +1,31 @@
+package labs.pm.data;
+
+public class ProductManagerException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7346050768603302179L;
+	
+	
+	public ProductManagerException() {
+		
+		super();
+		
+	}
+	
+	public ProductManagerException(String message) {
+	
+		super(message);
+		
+	}
+	
+	public ProductManagerException(String message, Throwable cause) {
+		
+		super(message, cause);
+		
+	}
+	
+	
+
+}
